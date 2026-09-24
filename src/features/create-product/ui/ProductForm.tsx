02c4dfeaa45aa42aaee446a3be7e5dc4a1e13ef9
BekzhanKutmanov <useruser19960718@gmail.com>
2026-09-24 11:00:09 +0600

    Установка базовых зависимостей. Структура компонента формы создания материалов

--- src/features/create-product/ui/ProductForm.tsx
+++ src/features/create-product/ui/ProductForm.tsx
@@ -1,14 +1,74 @@
-export function ProductForm() {
+import { TextField } from "@mui/material";
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import { Button } from '@mui/material';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
 
-    const renderForm = ()=> {
+
+export function ProductForm() {
+    // Стилизуем скрытый инпут
+    const VisuallyHiddenInput = styled('input')({
+        clip: 'rect(0 0 0 0)',
+        clipPath: 'inset(50%)',
+        height: 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        whiteSpace: 'nowrap',
+        width: 1,
+    });
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            console.log('Выбран файл:', file.name);
+            // Ваша логика сохранения файла в стейт формы
+        }
+    };
+
+    const renderForm = () => {
         return (
             <form>
+                <div className="">
+                    <div>
+                        <div>
+                            <h4>Название *</h4>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" size="small" />
+                        </div>
 
-                <div>
-                    <h4>Название *</h4>
-                    
+                        <div>
+                            <h4>Описание</h4>
+                            <TextareaAutosize
+                                aria-label="minimum height"
+                                minRows={3}
+                                placeholder="Minimum 3 rows"
+                                style={{ width: 200 }}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <Button
+                            component="label"
+                            role={undefined}
+                            variant="contained"
+                            tabIndex={-1}
+                            startIcon={<CloudUploadIcon />}
+                        >
+                            Загрузить фото
+                            <VisuallyHiddenInput
+                                type="file"
+                                accept="image/*" // Разрешаем только изображения
+                                onChange={handleFileChange}
+                            />
+                        </Button>
+                        <div>
+                            <h4>Цена за единицу *</h4>
+                            <TextField type="number" defaultValue={0} size="small" sx={{ fontSize: '13px' }} />
+                        </div>
+                    </div>
                 </div>
-
             </form>
         )
     }
@@ -21,6 +81,7 @@ export function ProductForm() {
                 <span>Заполните информацию о материале. Вы сможете отредактировать её позже.</span>
             </div>
 
+            {renderForm()}
 
         </div>
     );
--- src/pages/ProductManager.tsx
+++ src/pages/ProductManager.tsx
@@ -3,6 +3,10 @@ import { ProductForm } from "../features/create-product";
 export default function ProductManager () {
     return <div>
         Product manager
+        {/* Заполнение формы */}
         <ProductForm/>
+
+        {/* Список продуктов */}
+        
     </div>
 }
